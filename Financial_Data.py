b62c1db878747058524e7520047b99793def3c8e
kinyongs <kinyongs@naver.com>
2024-09-11 22:05:30 +0900

    Add files via upload

--- Financial_Data.py
+++ Financial_Data.py
@@ -38,16 +38,7 @@ page()
 # AdSense Script 삽입
 adsense_code = """
 <head>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3899080320104517"
-     data-ad-slot="3899080320104517"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<meta name="google-adsense-account" content="ca-pub-3899080320104517">
 </head>
 """
 
